ab85d485777e851e1d4bee372458f04d54f454d8
hehuimin <hehuimin@doweidu.com>
2017-09-26 16:44:04 +0800

    Initial Commit

---
+++ HHMTitleSizeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHMTitleSizeDemo
+//
+//  Created by Hehuimin on 2017/9/26.
+//  Copyright © 2017年 haoshiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHMTitleSizeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HHMTitleSizeDemo
+//
+//  Created by Hehuimin on 2017/9/26.
+//  Copyright © 2017年 haoshiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
